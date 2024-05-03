810a4fd69f985247321d8dd62e26d6b6cb379f83
Dardo Altobelli <altobellidardo@gmail.com>
2024-05-02 22:52:45 -0300

    add adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [auth()],
+  adapter: node({
+    mode: "standalone"
+  })
+});
