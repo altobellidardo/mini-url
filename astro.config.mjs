39a322502dde39a018686d99e9e2fe96d1f30675
Dardo Altobelli <altobellidardo@gmail.com>
2024-05-05 11:25:54 -0300

    add netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
 import node from "@astrojs/node";
-
 import db from "@astrojs/db";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [auth(), db()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
