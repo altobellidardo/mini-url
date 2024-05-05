22c588bde03eb6d35e02f6976d025de8a6c082ed
Dardo Altobelli <altobellidardo@gmail.com>
2024-05-05 11:00:06 -0300

    add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
-
 import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [auth()],
+  integrations: [auth(), db()],
   adapter: node({
     mode: "standalone"
   })
